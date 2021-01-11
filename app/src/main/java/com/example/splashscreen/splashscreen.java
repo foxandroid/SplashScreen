8ecfe33d0770fe486c7435e41e01776e157b194c
Robin Singla <robin.singla@asahiindia.com>
2021-01-11 08:34:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/splashscreen/splashscreen.java
@@ -0,0 +1,48 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class splashscreen extends AppCompatActivity {
+
+    private static int DELAY_TIME = 4000;
+
+    Animation topAnim, bottomAnim;
+    ImageView imageView;
+    TextView app_name;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen2);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        imageView = findViewById(R.id.imageView2);
+        app_name = findViewById(R.id.app_name);
+
+        imageView.setAnimation(topAnim);
+        app_name.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i = new Intent(splashscreen.this,MainActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        },DELAY_TIME);
+    }
+}
